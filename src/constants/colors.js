10f306c6cb653132ae52ebe8fee25dfe2f0ade68
Semen Verbych <joncolab@gmail.com>
2020-07-27 05:15:38 +0300

    Housekeeping (#6)
    * Add service worker
    * Update .gitignore to ignore IDE settings folder
    * Move colors to hsl and make related changes

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,10 @@
 export default {
-  white: 'white',
-  grey0: 'grey',
-  grey1: 'black',
-  red: 'red',
-  yellow: 'yellow',
+  white: 'hsl(0, 0%, 93%)',
+  grey0: 'hsl(0, 0%, 45%)',
+  grey1: 'hsl(212, 19%, 15%)',
+  blue0: 'hsl(196, 85%, 63%)',
+  blue1: 'hsl(212, 100%, 41%)',
+  red: 'hsl(357, 92%, 47%)',
+  yellow: 'hsl(44, 100%, 48%)',
+  green: 'hsl(74, 55%, 50%)',
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { serviceWorker } from './utils'
+
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+serviceWorker.register()
